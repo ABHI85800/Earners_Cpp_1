0713b5aa42e47818b68a62511621db26503e73b9
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-07-12 20:04:02 +0530

    Add files via upload

--- PatternPrinting/Diamond.cpp
+++ PatternPrinting/Diamond.cpp
@@ -4,6 +4,28 @@ int main(){
     int n;
     cout<<"Enter n: ";
     cin>>n;
+    // Method 2
+    int nsp = n-1, nst = 1;
+    for(int i=1;i<=2*n-1;i++){
+        for(int j=1;j<=nsp;j++){ // spaces
+            cout<<"  ";
+        }
+        for(int j=1;j<=nst;j++){ // stars
+            if(j==1 || j==nst) cout<<"* ";
+            else cout<<"  ";
+        }
+        if(i<n){
+            nsp--;
+            nst += 2;
+        }
+        else{
+            nsp++;
+            nst -= 2;
+        }
+        cout<<endl;
+    }
+
+
     // Method 1
     // // Upper Pyramid
     // int nsp = n-1, nst = 1;
@@ -32,24 +54,4 @@ int main(){
     //     nst -= 2;
     //     cout<<endl;
     // }
-
-    // Method 2
-    int nsp = n-1, nst = 1;
-    for(int i=1;i<=2*n-1;i++){
-        for(int j=1;j<=nsp;j++){ // spaces
-            cout<<"  ";
-        }
-        for(int j=1;j<=nst;j++){ // stars
-            cout<<"* ";
-        }
-        if(i<n){
-            nsp--;
-            nst += 2;
-        }
-        else{
-            nsp++;
-            nst -= 2;
-        }
-        cout<<endl;
-    }
 }
